5a4a2240b7bdeffa0bfa220f28979aaf8240f478
Tony Pujals <tpujals@gmail.com>
2023-08-07 01:02:35 -0700

    feat: lint and format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <p>@subfuzion/create-next-app</p>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center">
+            <p>@subfuzion/create-next-app</p>
+        </main>
+    );
 }
